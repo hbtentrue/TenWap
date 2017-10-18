c3d5b9ddb9325bf703730b5f164646c1e8c59f6d
longanD <783213408@qq.com>
2017-10-18 11:12:12 +0800

    首次提交到官网

---
+++ BenDi/BenDi/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BenDi.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return Content("你是否在git官网上看到了本地项目！");
+        }
+    }
+}
